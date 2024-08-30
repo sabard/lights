b963ba52f25c3e0d7512a24acc4ce91334d338e7
Sabar Dasgupta <s@bardasgupta.com>
2024-08-30 17:46:12 -0400

    switch between progs with midi

--- ruins/ruins.py
+++ ruins/ruins.py
@@ -9,12 +9,12 @@ from scipy import signal as sps
 
 # from .swirl import run_swirl
 
-# local
-STRIP_IP = "127.0.0.1"
-STRIP_PORT = 21324
+# # local
+# STRIP_IP = "127.0.0.1"
+# STRIP_PORT = 21324
 
-PAR_IP = "127.0.0.1"
-PAR_PORT = 21325
+# PAR_IP = "127.0.0.1"
+# PAR_PORT = 21325
 
 # studio
 # STRIP_IP = "172.17.3.41"
@@ -23,10 +23,19 @@ PAR_PORT = 21325
 # PAR_IP = "172.17.3.43"
 # PAR_PORT = 21324
 
+# shaker
+STRIP_IP = "192.168.88.21"
+STRIP_PORT = 21324
+
+PAR_IP = "192.168.88.20"
+PAR_PORT = 21324
+
+
 TIMEOUT = 2
 PYGAME_MIDI_DEVICE=0
 
-num_pars = 4
+NUM_PARS = 4
+NUM_STRIP_LEDS = 16
 
 # red_val = 255
 # green_val = 255
@@ -96,13 +105,13 @@ def set_segment_par(start, end, r, g, b, w, print_packet=False):
     sock.sendto(m, (PAR_IP, PAR_PORT))
 
 
-def set_segment_strip(start, end, r, g, b, print_packet=False):#, w):
+def set_segment_strip(start, end, r, g, b, w=None, print_packet=False):
     m = []
-    m.append(1) # WARLS
+    m.append(3) # DRGBW
     m.append(TIMEOUT)
 
     for i in range(start, end):
-        m.append(i)  # Index of pixel to change
+        # m.append(i)  # Index of pixel to change
 
          # pixel red value
         if isinstance(r, Iterable):
@@ -121,6 +130,13 @@ def set_segment_strip(start, end, r, g, b, print_packet=False):#, w):
             m.append(b[i])
         else:
             m.append(b)
+
+        # pixel white value
+        if w is not None:
+            if isinstance(b, Iterable):
+                m.append(b[i])
+            else:
+                m.append(b)
 
         # m.append(w)  # pixel white value
 
@@ -143,7 +159,7 @@ def on_press(key):
         return
 
     start_idx = 0
-    end_idx = num_pars - 1
+    end_idx = NUM_PARS - 1
     print("on: ", start_idx, end_idx)
     set_segment_par(start_idx, end_idx, red_val, green_val, blue_val, white_val)
 
@@ -155,7 +171,7 @@ def on_release(key):
         return
 
     start_idx = 0
-    end_idx = num_pars - 1
+    end_idx = NUM_PARS - 1
     print("off: ", start_idx, end_idx)
     set_segment_par(start_idx, end_idx, 0, 0, 0, 0)
 
@@ -191,47 +207,6 @@ mIn = MidiKeyboard()
 # listener = keyboard.Listener(on_press=on_press, on_release=on_release)
 # listener.start()
 
-def run_slowfade():
-    global i,j,k,signal
-
-    while True:
-        if mIn.midiDevice:
-            if mIn.midiDevice.poll():
-                events = mIn.midiDevice.read(1)
-                ev_data = events[0][0]
-                button = ev_data[1]
-                ev_value = ev_data[2]
-
-                if button == 14:
-                    red_val = ev_value * 2
-                elif button == 15:
-                    green_val = ev_value * 2
-                elif button == 16:
-                    blue_val = ev_value * 2
-                elif button == 17:
-                    white_val = ev_value * 2
-        else:
-            red_val = int((signal[i] + 1.) * 127.)
-            green_val = int((signal[j] + 1.) * 127.)
-            blue_val = int((signal[k] + 1.) * 127.)
-            white_val = 0
-
-            red_val, green_val, blue_val = reduce_white(red_val, green_val, blue_val, 1.)
-            red_val, green_val, blue_val = reduce_white(red_val, green_val, blue_val, .25)
-
-            i += 1
-            j += 2
-            k += 3
-            if i >= len(t):
-                i = 0
-            if j >= len(t):
-                j = 0
-            if k >= len(t):
-                k = 0
-            set_segment_par(0, num_pars, red_val, green_val, blue_val, white_val)
-            time.sleep(.05)
-
-
 class Par:
     def __init__(self, r, g, b, w):
         self.r = r
@@ -252,12 +227,17 @@ def make_array(color, num, pre=np.empty(0), post=np.empty(0)):
 def random_color(max_val=255):
     return int(np.random.random() * max_val)
 
-def random_rgbw(max_val=255, white=False):
+def random_rgbw(max_val=255, white=False, reduce_white=False):
     white_val = random_color(max_val) if white else 0
+    rgb_color = (
+        random_color(max_val), # r
+        random_color(max_val), # g
+        random_color(max_val), # b
+    )
+    if reduce_white:
+        rgb_color = reduce_white(rgb_color, 1)
     return np.array([
-            random_color(max_val), # r
-            random_color(max_val), # g
-            random_color(max_val), # b
+            *rgb_color,
             white_val # w
         ])
 
@@ -270,7 +250,10 @@ def fade_colors(m1, m2, num_sends, send_rate):
         c = calculate_color(m1, m2, i, num_sends)
 
         # send color
-        set_segment_par(0, num_pars, c[:,0], c[:,1], c[:,2], c[:,3])
+        set_segment_par(0, NUM_PARS, c[:,0], c[:,1], c[:,2], c[:,3])
+
+
+        set_segment_strip(0, NUM_STRIP_LEDS, 255, 0, 0, 0)
 
         # wait
         time.sleep(send_rate)
@@ -290,20 +273,96 @@ def create_shuffled_eye(ndim, shuffle_white=False):
         return eye
 
 
+def check_midi():
+    if mIn.midiDevice:
+        if mIn.midiDevice.poll():
+            events = mIn.midiDevice.read(1)
+            ev_data = events[0][0]
+            button = ev_data[1]
+            ev_value = ev_data[2]
+
+            print(events)
+
+            if button == 67:
+                if ev_value == 127:
+                    return True
+            elif button == 64:
+                if ev_value == 127:
+                    return False
+
+    return None
+
+
+def run_slowfade():
+    global i,j,k,signal
+
+    print("RUN SLOWFADE")
+
+    while True:
+
+        # if mIn.midiDevice:
+        #     if mIn.midiDevice.poll():
+        #         events = mIn.midiDevice.read(1)
+        #         ev_data = events[0][0]
+        #         button = ev_data[1]
+        #         ev_value = ev_data[2]
+
+        #         if button == 14:
+        #             red_val = ev_value * 2
+        #         elif button == 15:
+        #             green_val = ev_value * 2
+        #         elif button == 16:
+        #             blue_val = ev_value * 2
+        #         elif button == 17:
+        #             white_val = ev_value * 2
+
+        next_preset = check_midi()
+        if next_preset is not None:
+            return next_preset
+
+        else:
+            red_val = int((signal[i] + 1.) * 127.)
+            green_val = int((signal[j] + 1.) * 127.)
+            blue_val = int((signal[k] + 1.) * 127.)
+            white_val = 0
+
+            red_val, green_val, blue_val = reduce_white(red_val, green_val, blue_val, 1.)
+            red_val, green_val, blue_val = reduce_white(red_val, green_val, blue_val, .25)
+
+            i += 1
+            j += 2
+            k += 3
+            if i >= len(t):
+                i = 0
+            if j >= len(t):
+                j = 0
+            if k >= len(t):
+                k = 0
+            set_segment_par(0, NUM_PARS, red_val, green_val, blue_val, white_val)
+            time.sleep(.05)
+
+
 def run_separate(num_seconds=10, send_rate=.2):
+
+    print("RUN SEPARATE")
 
     num_sends = int(1.0 * num_seconds / send_rate)
     num_colors = 4 # rgbw
 
-    shuffled_eye = create_shuffled_eye(num_pars)
+    shuffled_eye = create_shuffled_eye(NUM_PARS)
 
     first = True
     m_rgb2 = None
 
     while True:
+
+        next_preset = check_midi()
+        if next_preset is not None:
+            return next_preset
+
         # pick color
         start_c = random_rgbw()
-        m_start_c = np.tile(start_c, num_pars).reshape(num_colors, num_pars).dot(shuffled_eye)
+        m_start_c = np.tile(start_c, NUM_PARS).reshape(num_colors, NUM_PARS).dot(shuffled_eye)
         # TODO check for mismatched colors/pars
         m_rgb1 = m_start_c * shuffled_eye
 
@@ -324,10 +383,10 @@ def run_separate(num_seconds=10, send_rate=.2):
         print("start color to rgb1")
         fade_colors(m_start_c, m_rgb1, num_sends, send_rate)
 
-        shuffled_eye = create_shuffled_eye(num_pars)
+        shuffled_eye = create_shuffled_eye(NUM_PARS)
 
         end_c = random_rgbw()
-        m_end_c =  np.tile(end_c, num_pars).reshape(num_colors, num_pars).dot(shuffled_eye)
+        m_end_c =  np.tile(end_c, NUM_PARS).reshape(num_colors, NUM_PARS).dot(shuffled_eye)
         m_rgb2 = m_end_c * shuffled_eye
 
         # fade from rgb1 to rgb2
@@ -345,36 +404,44 @@ def run_separate(num_seconds=10, send_rate=.2):
 
 
 def run_swirl(colors=((255, 165, 0, 0),(0, 0, 255, 0))):
-    num_pars = 4
+
+    print("RUN SWIRL")
+
+    NUM_PARS = 4
     step = 0.001
 
     i = -1
     t = np.linspace(0, 2*np.pi, 100)
 
     c1_val = 0.01
-    c2_val = (num_pars / 2.) + c1_val
+    c2_val = (NUM_PARS / 2.) + c1_val
 
     while True:
+
+        next_preset = check_midi()
+        if next_preset is not None:
+            return next_preset
+
         i += 1
         if i >= len(t):
             i = 0
-        if i + num_pars <= len(t):
-            t_cur = t[i:i+num_pars]
+        if i + NUM_PARS <= len(t):
+            t_cur = t[i:i+NUM_PARS]
         else:
-            t_cur = t[i:] + t[:i+num_pars-len(t)]
+            t_cur = t[i:] + t[:i+NUM_PARS-len(t)]
 
 
         # triang = sps.windows.triang(t_cur)
 
-        m_c1 = np.tile(colors[0], (num_pars, 1))
-        m_c2 = np.tile(colors[1], (num_pars, 1))
+        m_c1 = np.tile(colors[0], (NUM_PARS, 1))
+        m_c2 = np.tile(colors[1], (NUM_PARS, 1))
 
         c1_i1 = int(np.floor(c1_val))
         c1_i2 = int(np.ceil(c1_val))
         c1_v1 = c1_val - c1_i1
         c1_v2 = c1_i2 - c1_val
-        c1_i2 %= num_pars
-        c1_diag = np.zeros(num_pars)
+        c1_i2 %= NUM_PARS
+        c1_diag = np.zeros(NUM_PARS)
         c1_diag[c1_i1] = c1_v1
         c1_diag[c1_i2] = c1_v2
         c1_diag  = np.diag(c1_diag)
@@ -383,28 +450,52 @@ def run_swirl(colors=((255, 165, 0, 0),(0, 0, 255, 0))):
         c2_i2 = int(np.ceil(c2_val))
         c2_v1 = c2_val - c2_i1
         c2_v2 = c2_i2 - c2_val
-        c2_i2 %= num_pars
-        c2_diag = np.zeros(num_pars)
+        c2_i2 %= NUM_PARS
+        c2_diag = np.zeros(NUM_PARS)
         c2_diag[c2_i1] = c2_v1
         c2_diag[c2_i2] = c2_v2
         c2_diag  = np.diag(c2_diag)
 
         m = (c1_diag.dot(m_c1) + c2_diag.dot(m_c2)).astype(int)
 
-        set_segment_par(0, num_pars, m[:,0], m[:,1], m[:,2], m[:,3])
+        set_segment_par(0, NUM_PARS, m[:,0], m[:,1], m[:,2], m[:,3])
         time.sleep(.05)
 
         c1_val += step
         c2_val += step
 
-        if c1_val >= num_pars:
-            c1_val -= num_pars
+        if c1_val >= NUM_PARS:
+            c1_val -= NUM_PARS
 
-        if c2_val >= num_pars:
-            c2_val -= num_pars
+        if c2_val >= NUM_PARS:
+            c2_val -= NUM_PARS
 
 
-# run_slowfade()
-run_separate()
-# run_swirl()
+presets = [
+    run_slowfade,
+    run_separate,
+    run_swirl
+]
+
+preset_idx = 0
+def run_next(to_next=None):
+    global preset_idx
+
+    if to_next is None:
+        return run_slowfade()
+    elif to_next:
+        preset_idx += 1
+        if preset_idx == len(presets):
+            preset_idx = 0
+    else:
+        preset_idx -= 1
+        if preset_idx < 0:
+            preset_idx = len(presets) -1
+
+    return presets[preset_idx]()
+
+
+to_next = None
+while True:
+    to_next = run_next(to_next)
 
